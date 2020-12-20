d2a36a0ec26576f33d75f2fabef9e159274a9484
Giannis Anastasopoulos <giannis.anastasopoulos@sodaq.com>
2020-12-20 23:14:28 +0100

    Pull down input pin so that it does not float

--- firmware/Core/Src/main.c
+++ firmware/Core/Src/main.c
@@ -388,7 +388,7 @@ static void MX_GPIO_Init(void)
     /*Configure GPIO pin : INPUT_PIN_Pin */
     GPIO_InitStruct.Pin = INPUT_PIN_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Pull = GPIO_PULLDOWN;
     HAL_GPIO_Init(INPUT_PIN_GPIO_Port, &GPIO_InitStruct);
 }
 
